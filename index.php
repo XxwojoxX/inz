5081534c21edebaa27a100395ca08db9da3ac527
Wojciech Aleksander Gabryś <wojciech.gabrys@student.po.edu.pl>
2023-07-06 13:13:28 +0200

    poprawki wizualne, ciag dalszy php

--- index.php
+++ index.php
@@ -13,120 +13,109 @@
 	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
 
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-	<script src="scripts/clock.js"></script>
 
     <title>ZOO</title>
 </head>
 <body>
+	<?php
+		if(isset($_SESSION['username']))
+		{
+			echo $_SESSION['username'];
+			echo '<a href="PHP/logout.php"><button class="btn btn-slide-left">Wyloguj</button></a>';
+		}
+		else
+		{
+		echo '<nav class="nav" id="nav">
+				<a href="login_form.php">
+					<button class="nav-button" id="nav-button">
+						Zaloguj
+						<div class="arrow-wraper">
+							<div class="arrow"></div>
+						</div>
+					</button>
+				</a>
 
-<div class="site-container"id="site-container">
-
-
-
-<div class="container" id="container">
-			<p>
-				<?php
-					if(isset($_SESSION['username']))
-					{
-						echo $_SESSION['username'];
-						echo '<a href="PHP/logout.php"><button class="btn btn-slide-left">Wyloguj</button></a>';
-					}
-					else
-					{
-						echo '<nav class="nav" id="nav">
-								<a href="login_form.php">
-									<button class="nav-button" id="nav-button">
-										Zaloguj
-										<div class="arrow-wraper">
-											<div class="arrow"></div>
-										</div>
-								</button>
-								</a>
-
-								<a href="register.html">
-									<button class="nav-button" id="nav-button">
-										Zarejestruj
-										<div class="arrow-wraper">
-											<div class="arrow"></div>
-										</div>
-									</button>
-								</a>
-							</nav>';
+				<a href="register_form.php">
+					<button class="nav-button" id="nav-button">
+						Zarejestruj
+						<div class="arrow-wraper">
+							<div class="arrow"></div>
+						</div>
+					</button>
+				</a>
+			</nav>';
 					}
 				?>
 			</p>
-		</div>
 
+	<div class="site-container"id="site-container">
+		<br>
+		<br>
+		<div class="section-container" id="section-container">
+			<div class="section" id="section">
 
-    
+				<div class="ui-card" id="ui-card">
+					<img src="images/1.png" alt="wstaw zdjecie strony glownej">
+					<div class="description">
+						<h3>Strona główna</h3>
+						<h3>krotki opis</h3>
+						<a href="index.php">Zobacz</a>
+					</div>
+				</div>
 
-    <br>
-    <br>
-    
-	<div class="section-container" id="section-container">
-		<div class="section" id="section">
-			<div class="ui-card" id="ui-card">
-				<img src="images/1.png" alt="wstaw zdjecie strony glownej">
-				<div class="description">
-					<h3>Strona główna</h3>
-					<h3>krotki opis</h3>
-					<a href="#">Zobacz</a>
+				<div class="ui-card">
+					<img src="images/1.png" alt="wstaw zdjecie sklepu">
+					<div class="description">
+						<h3>Sklep</h3>
+						<h3>krotki opis</h3>
+						<a href="shop.php">Zobacz</a>
+					</div>
 				</div>
-			</div>
 
-			<div class="ui-card">
-				<img src="images/1.png" alt="wstaw zdjecie sklepu">
-				<div class="description">
-					<h3>Sklep</h3>
-					<h3>krotki opis</h3>
-					<a href="#">Zobacz</a>
+				<div class="ui-card">
+					<img src="images/1.png" alt="wstaw zdjecie aktualnosci">
+					<div class="description">
+						<h3>Aktualności</h3>
+						<h3>krotki opis</h3>
+						<a href="news.php">Zobacz</a>
+					</div>
 				</div>
-			</div>
-			<div class="ui-card">
-				<img src="images/1.png" alt="wstaw zdjecie aktualnosci">
-				<div class="description">
-					<h3>Aktualności</h3>
-					<h3>krotki opis</h3>
-					<a href="#">Zobacz</a>
+
+				<div class="ui-card">
+					<img src="images/1.png" alt="wstaw mieszanke zdjec zwierzat">
+					<div class="description">
+						<h3>Zwierzęta</h3>
+						<h3>krotki opis</h3>
+						<a href="animals.php">Zobacz</a>
+					</div>
 				</div>
-			</div>
-			<div class="ui-card">
-				<img src="images/1.png" alt="wstaw mieszanke zdjec zwierzat">
-				<div class="description">
-					<h3>Zwierzęta</h3>
-					<h3>krotki opis</h3>
-					<a href="#">Zobacz</a>
+
+				<div class="ui-card">
+					<img src="images/1.png" alt="wstaw wykadrowany kalendarz">
+					<div class="description">
+						<h3>Wydarzenia</h3>
+						<h3>krotki opis</h3>
+						<a href="#events.php">Zobacz</a>
+					</div>
 				</div>
-			</div>
-			<div class="ui-card">
-				<img src="images/1.png" alt="wstaw wykadrowany kalendarz">
-				<div class="description">
-					<h3>Wydarzenia</h3>
-					<h3>krotki opis</h3>
-					<a href="#">Zobacz</a>
-				</div>
-			</div>
-			<div class="ui-card">
-				<img src="images/1.png" alt="wstaw zdjecie z wieloma numerami/mailami">
-				<div class="description">
-					<h3>Kontakt</h3>
-					<h3>krotki opis</h3>
-					<a href="contact.html">Zobacz</a>
+
+				<div class="ui-card">
+					<img src="images/1.png" alt="wstaw zdjecie z wieloma numerami/mailami">
+					<div class="description">
+						<h3>Kontakt</h3>
+						<h3>krotki opis</h3>
+						<a href="contact.php">Zobacz</a>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-
-
-
-	</div>
-
 
 	<footer>
-		<a href="index.html"><img src="images/favicon.ico"></a>
+		<a href="index.php"><img src="images/favicon.ico"></a>
 
 		<div class="card">
-			<a class="socialContainer containerOne" href="#">
+			<a class="socialContainer containerOne" href="https://www.instagram.com/">
 			  <svg viewBox="0 0 16 16" class="socialSvg instagramSvg"> <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"></path> </svg>
 			</a>
 			
